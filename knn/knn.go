547bcc6d6dc5c1e1b37aeb43547e09016c97caf3
Bohdan Bodnaruk <bohdan.bodnaruk@gmail.com>
2016-03-04 01:17:02 +0100

    knn

---
+++ knn/knn.go
@@ -0,0 +1,79 @@
+package knn
+
+import (
+    // "fmt"
+    "errors"
+    "math"
+    "sort"
+)
+
+type DiffArray struct {
+    Values []float64
+    Indices []int
+}
+
+func NewDiffArray(len int) DiffArray {
+    da := DiffArray{
+        make([]float64, len),
+        make([]int, len),
+    }
+    for i := 0; i < len; i++ {
+        da.Indices[i] = i;
+    }
+    return da
+}
+
+func (da DiffArray) Len() int {
+    return len(da.Values)
+}
+
+func (da DiffArray) Less(i, j int) bool {
+    return da.Values[i] < da.Values[j]
+}
+
+func (da DiffArray) Swap(i, j int) {
+    da.Values[i], da.Values[j] = da.Values[j], da.Values[i]
+    da.Indices[i], da.Indices[j] = da.Indices[j], da.Indices[i]
+}
+
+func Classify( inX []float64, dataSet [][]float64, labels []string, k int ) (string, error) {
+    if len(dataSet) == 0 {
+        return "", errors.New("Empty data set")
+    }
+    diff := calcDiff(inX, dataSet)
+    sort.Sort(diff)
+    // fmt.Println(diff)
+
+    return vote(labels, diff, k), nil
+}
+
+func calcDiff(inX []float64, dataSet [][]float64) DiffArray{
+    res := NewDiffArray(len(dataSet))
+    // fmt.Println(res)
+    for i, row := range dataSet {
+        // fmt.Printf("%#v %d", row, i)
+        for j := 0; j < len(inX); j++ {
+            res.Values[i] += math.Pow(inX[j]*inX[j] - row[j]*row[j], 2)
+        }
+        res.Values[i] = math.Sqrt(res.Values[i])
+    }
+    return res
+}
+
+func vote(labels []string, diff DiffArray, k int) string {
+    m := make(map[string]int)
+    for i := 0; i < int(math.Min( float64(len(diff.Values)), float64(k)) ); i++ {
+        m[labels[diff.Indices[i]]] += 1
+    }
+
+    var max int
+    var res string
+    for k, v := range m {
+        if v > max {
+            max = v
+            res = k
+        }
+    }
+
+    return res
+}
