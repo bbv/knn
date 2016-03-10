ff7b180bb308b15f7a50a07ad3231180f326ceaf
Bohdan Bodnaruk <bohdan.bodnaruk@gmail.com>
2016-03-10 01:12:08 +0100

    decision trees

---
+++ decision_trees/decision_trees.go
@@ -0,0 +1,22 @@
+package decisionTrees
+
+import (
+    // "fmt"
+    "math"
+)
+
+func CalcShannonEntropy(dataSet [][]int) float64 {
+    numEntries := len(dataSet)
+    labelCounts := make(map[int]int)
+    for i := 0; i < numEntries; i++ {
+        currentLabel := dataSet[i][len(dataSet[i]) - 1]
+        labelCounts[currentLabel] += 1
+    }
+
+    shannonEnt := 0.0
+    for _, count := range labelCounts {
+        prob := float64(count) / float64(numEntries)
+        shannonEnt -= prob * math.Log2(prob)
+    }
+    return shannonEnt
+}
